adfc7a3e335d5a342a31f961c513fcf789473dfa
azizbekjuraev <ajuraev98@mail.ru>
2026-03-10 14:49:37 +0500

    add Swagger docs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,38 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiBody,
+  ApiProperty
+} from '@nestjs/swagger'
 
+class CreateUserDto {
+  @ApiProperty({ example: "admin", description: "User name" })
+  username: string;
+
+  @ApiProperty({ example: "Admin@Secure#2024", description: "User password" })
+  password: string
+}
+
+@ApiTags('auth')
+@ApiBearerAuth()
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) { }
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
+  @ApiOperation({ summary: 'User login' })
+  @ApiResponse({ status: 200, description: "OK" })
+  @ApiResponse({ status: 401, description: "Unauthorized." })
+  @ApiBody({
+    type: CreateUserDto
+  })
 
-  // add dto here instead of string/any
-
-  signIn(@Body() signInDto: Record<string, any>) {
+  signIn(@Body() signInDto: CreateUserDto) {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = Number(configService.get('PORT'));
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("Prosight Application")
+    .setDescription("Docs")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
   await app.listen(port);
 }
 bootstrap();
